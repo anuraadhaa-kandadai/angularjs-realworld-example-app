e0e4e774d3095e431741935bb863b9b38c370702
anuraadhaa-kandadai <55108044+anuraadhaa-kandadai@users.noreply.github.com>
2026-06-17 20:06:44 -0500

    fix(home): Article feed does not reload after user logs in [TP-102]
    - Add $scope.$watch to monitor User.current authentication state changes
    - Automatically switch from 'all' to 'feed' when user logs in
    - Automatically switch from 'feed' to 'all' when user logs out
    - Broadcast list configuration changes to refresh article list component
    - Fixes race condition where feed type was only set at initialization
    
    Resolves: TP-102

--- src/js/home/home.controller.js
+++ src/js/home/home.controller.js
@@ -4,6 +4,7 @@ class HomeCtrl {
 
     this.appName = AppConstants.appName;
     this._$scope = $scope;
+    this._User = User;
 
     // Get list of all tags
     Tags
@@ -15,10 +16,26 @@ class HomeCtrl {
         }
       );
 
-    // Set current list to either feed or all, depending on auth status.
+    // Set initial list configuration based on current auth status
     this.listConfig = {
       type: User.current ? 'feed' : 'all'
     };
+
+    // Watch for authentication state changes
+    $scope.$watch(() => this._User.current, (newUser, oldUser) => {
+      // Only update if authentication state actually changed
+      if ((!!newUser) !== (!!oldUser)) {
+        const newType = newUser ? 'feed' : 'all';
+        
+        // Update list configuration and refresh the feed
+        this.listConfig = {
+          type: newType
+        };
+        
+        // Broadcast the change to update the article list
+        this.changeList(this.listConfig);
+      }
+    });
 
   }
 
